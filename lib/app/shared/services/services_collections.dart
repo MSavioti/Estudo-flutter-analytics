988bd5f9eaed5934174174a5d927e7ebff259420
angelo-squadra <angelo.savioti@squadra.com.br>
2022-02-18 18:08:28 -0300

    created fake service for retrieving collections

---
+++ lib/app/shared/services/services_collections.dart
@@ -0,0 +1,28 @@
+import 'package:estudo_flutter_analytics/app/shared/models/collection.dart';
+
+class ServiceCollections {
+  ServiceCollections._();
+
+  static ServiceCollections? _instance;
+
+  static ServiceCollections get instance =>
+      _instance ??= ServiceCollections._();
+
+  Future<List<Collection>> getCollections(int pageSize) async {
+    await Future.delayed(const Duration(seconds: 2));
+
+    final List<Collection> _collections = [];
+
+    for (var i = 0; i < pageSize; i++) {
+      final _collection = Collection(
+        id: i,
+        name: 'Collection $i',
+        image: 'https://picsum.photos/100?random=${i + 1}',
+        products: ['1', '2', '3', '4', '5'],
+      );
+      _collections.add(_collection);
+    }
+
+    return _collections;
+  }
+}
