4ca065f2e7a72fc455b774a6291059632c074ec7
angelo-squadra <angelo.savioti@squadra.com.br>
2022-02-18 17:28:00 -0300

    creatd base classes

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:estudo_flutter_analytics/app/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      theme: ThemeData(
+        primaryColor: Colors.purple,
+      ),
+    );
+  }
+}
