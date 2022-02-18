16aa139ec6b978d2143e46859e74bc82a79e78be
angelo-squadra <angelo.savioti@squadra.com.br>
2022-02-18 18:09:43 -0300

    list of collections

--- lib/app/pages/home_page.dart
+++ lib/app/pages/home_page.dart
@@ -1,10 +1,52 @@
+import 'package:estudo_flutter_analytics/app/shared/models/collection.dart';
+import 'package:estudo_flutter_analytics/app/shared/services/services_collections.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Collection> collections = [];
+
+  @override
+  void initState() {
+    loadCollections();
+    super.initState();
+  }
+
+  void loadCollections() async {
+    final _collections = await ServiceCollections.instance.getCollections(5);
+    setState(() {
+      collections = _collections;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Home')),
+      body: collections.isEmpty
+          ? const Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              padding: const EdgeInsets.symmetric(vertical: 8.0),
+              itemCount: collections.length,
+              itemBuilder: (context, i) {
+                return Card(
+                  child: ListTile(
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 8.0,
+                      horizontal: 8.0,
+                    ),
+                    leading: Image.network(collections[i].image),
+                    title: Text(collections[i].name),
+                  ),
+                );
+              },
+            ),
+    );
   }
 }
