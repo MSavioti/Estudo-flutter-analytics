a27b0e5ce4738a93acd2635dbc5636c5f5e60bc5
angelo-squadra <angelo.savioti@squadra.com.br>
2022-02-18 18:24:49 -0300

    added routes

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,6 @@
+import 'package:estudo_flutter_analytics/app/pages/collection_page.dart';
 import 'package:estudo_flutter_analytics/app/pages/home_page.dart';
+import 'package:estudo_flutter_analytics/app/pages/product_page.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -14,6 +16,8 @@ class App extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => const HomePage(),
+        '/collection': (context) => const CollectionPage(),
+        '/product': (context) => const ProductPage(),
       },
     );
   }
