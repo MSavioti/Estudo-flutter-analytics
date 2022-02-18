3c07d65884334d1fe7bb12740aa229c71422ad74
angelo-squadra <angelo.savioti@squadra.com.br>
2022-02-18 18:08:54 -0300

    changed products list to potential sku list

--- lib/app/shared/models/collection.dart
+++ lib/app/shared/models/collection.dart
@@ -1,10 +1,8 @@
-import 'package:estudo_flutter_analytics/app/shared/models/product.dart';
-
 class Collection {
   final int id;
   final String name;
   final String image;
-  final List<Product> products;
+  final List<String> products;
 
   Collection({
     required this.id,
